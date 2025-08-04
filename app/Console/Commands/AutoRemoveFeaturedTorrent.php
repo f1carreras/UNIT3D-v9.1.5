daed408ef01559ec88c81820e4ca9594bdfadd49
f1carreras <f1carreras1985@gmail.com>
2025-08-04 12:31:51 -0400

    Featured Seteo Inicial

--- app/Console/Commands/AutoRemoveFeaturedTorrent.php
+++ app/Console/Commands/AutoRemoveFeaturedTorrent.php
@@ -57,7 +57,7 @@ class AutoRemoveFeaturedTorrent extends Command
     final public function handle(): void
     {
         $current = Carbon::now();
-        $featuredTorrents = FeaturedTorrent::with('torrent')->where('created_at', '<', $current->copy()->subDays(7))->get();
+        $featuredTorrents = FeaturedTorrent::with('torrent')->where('created_at', '<', $current->copy()->subDays(4))->get();
 
         foreach ($featuredTorrents as $featuredTorrent) {
             // Find The Torrent
